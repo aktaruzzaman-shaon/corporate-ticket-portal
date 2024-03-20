ec3b3709c25d0f285871eb941b00b6c8c5e284bd
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2024-03-20 16:02:55 +0600

    navbar and ticket form iniital update

--- app/(components)/shared/nav.js
+++ app/(components)/shared/nav.js
@@ -1,8 +1,29 @@
 "use client"
 const Nav = () => {
     return (
-        <div>
-            
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">daisyUI</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Open</a></li>
+                    <li><a>ALL</a></li>
+                    <li><a>Closed</a></li>
+                    <li><a>Dashboard</a></li>
+                    <li>
+                        <details>
+                            <summary>
+                                Account
+                            </summary>
+                            <ul className="p-2 bg-base-100 rounded-t-none">
+                                <li><a>Login</a></li>
+                                <li><a>Settings</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
---
+++ app/(components)/shared/ticketForm.js
@@ -0,0 +1,82 @@
+"use client"
+import React, { useState } from 'react'
+
+const TicketForm = () => {
+
+    const singleTicket = {
+        title: "",
+        description: "",
+        category: "hardware problem",
+        priority: 1,
+        progress: 0,
+        status: "not started"
+    }
+
+    const [newTicket, setNewTicket] = useState(singleTicket);
+
+    const handleForm = (formData) => {
+        const name = formData.get("title");
+        console.log(name);
+    }
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setNewTicket((prevState) => (
+            {
+                ...prevState,
+                [name]: value,
+            }
+        ))
+    }
+    const handleSubmit = () => {
+        e.preventDefault();
+        console.log(newTicket)
+    }
+
+    return (
+        <div className='flex justify-center'>
+            <form onSubmit={handleSubmit}>
+                <h3 className='text-center'>Create your ticket</h3>
+
+                <label>Title</label>
+                <input type="text" id="title" name="title" placeholder="Title" onChange={handleChange} className="input w-full max-w-xs" />
+                <br />
+
+                <label>Description</label>
+                <input type="text" id="description" name="description" placeholder="Description" onChange={handleChange} className="input w-full max-w-xs" />
+                <br />
+
+                <label className="mr-5"> Category</label>
+                <select name="category" id="category" onChange={handleChange} value={newTicket.category}>
+                    <option value="hardware problem">Hardware problem</option>
+                    <option value="software problem">software problem</option>
+                    <option value="network problem">network problem</option>
+                </select>
+                <br />
+
+                <label>Priority  : </label>
+                <div>
+                    <input type="radio" name="priority" id="priority-1" className="radio radio-primary" value={1} onChange={handleChange} checked={newTicket.priority == 1} />
+                    <label>1</label>
+                    <input type="radio" name="priority" id="priority-1" className="radio radio-primary" onChange={handleChange} checked={newTicket.priority == 2} />
+                    <label>2</label>
+                    <input type="radio" name="priority" id="priority-1" className="radio radio-primary" onChange={handleChange} checked={newTicket.priority == 3} />
+                    <label>3</label>
+                </div>
+                <br />
+
+                <label>Progress</label><br />
+                <input type="range" id="progress" name="progress" min={0} max={100} className='w-full' onChange={handleChange} value={newTicket.progress} />
+                <br />
+
+                <label>Status</label>
+                <input type="text" id="status" name="status" placeholder="Status" value={newTicket.status} className="input w-full max-w-xs" /><br />
+
+                <button type='submit' className='btn'>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default TicketForm;
